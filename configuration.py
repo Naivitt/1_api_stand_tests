6a582e5d960978dd546e9a27a88cee040b337155
Naivitt Parra Garay <misticssss@Naivitts-MacBook-Pro.local>
2024-08-17 19:09:15 +1000

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-8e1cd547-dc3f-4cbb-b552-892119578137.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
